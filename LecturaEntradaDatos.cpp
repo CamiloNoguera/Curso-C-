0646d98b437abb8216bf739fc74bb45bf4881c2b
CamiloNoguera <85818714+CamiloNoguera@users.noreply.github.com>
2021-12-28 15:53:56 -0500

    Add files via upload

--- LecturaEntradaDatos.cpp
+++ LecturaEntradaDatos.cpp
@@ -1,1 +1,15 @@
 // Lectura o entrada de datos
+// NOTA: NO USAR TILDES NI CARACTERES ESPECIALES
+
+#include<iostream>
+using namespace std;
+
+int main(){
+	
+	float numero; // Definimos la variable
+	cout<<"Digite un numero: ";
+	cin>>numero; // Guardamos la variable
+	
+	cout<<"\nEl numero que digito es: "<<numero;
+	return 0;
+}
