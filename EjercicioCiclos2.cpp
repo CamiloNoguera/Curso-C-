d4172776df1fd1ca645fc8d0d7ccf2281d9fc037
CamiloNoguera <85818714+CamiloNoguera@users.noreply.github.com>
2022-01-03 18:14:21 -0500

    Ejercicio 26

---
+++ EjercicioCiclos2.cpp
@@ -0,0 +1,3 @@
+/* 26. Realice un programa que lea de la entrada estandar numeros hasta que se introduzca un cero.
+En ese momento el programa debe terminar y mostrar en la salida estandar 
+el numero de valores mayores que cero leidos.*/
