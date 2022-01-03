d619bd4998dfb04cc174eddd0afe1d3f823a7e03
CamiloNoguera <85818714+CamiloNoguera@users.noreply.github.com>
2022-01-03 17:53:33 -0500

    Ciclos o bucles 3

---
+++ For.cpp
@@ -0,0 +1,19 @@
+/*La sentecia for
+	for(expr1; expresion logica; expre2){
+		conjunto de instrucciones;
+	}
+*/
+
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+int main(){
+	
+	for(int i=1; i<=10; i++){
+		cout<<i<<endl;
+	}
+	
+	getch();
+	return 0;
+}
